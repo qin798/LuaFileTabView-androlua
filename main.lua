02644bae3d99ac0564b27658b9b7da3a39b54989
秦人 <121938003+qin798@users.noreply.github.com>
2024-01-22 00:56:39 +0800

    Add files via upload

--- main.lua
+++ main.lua
@@ -5,10 +5,34 @@ import "android.widget.*"
 import "android.view.*"
 import "com.google.android.material.tabs.TabLayout"
 import "java.io.File"
+import "vinx.material.textfield.MaterialTextField"
+import "com.google.android.material.button.MaterialButton"
+import "com.google.android.material.checkbox.MaterialCheckBox"
+MDC_R = luajava.bindClass "com.google.android.material.R"
+LuaThemeUtil = luajava.newInstance("github.daisukiKaffuChino.utils.LuaThemeUtil",activity)
 
-activity.setContentView(loadlayout(require"layout"))
+activity.setTitle("LuaFileTabView")
+.setContentView(loadlayout(require"layout"))
 
-mTab.setPath("/storage/emulated/0/Android/data/com.tencent.mobileqq/Tencent/QQfile_recv/nt_qq_f031d2c4cdb29dce5de1f9f465897ecd/Video/Thumb")
-button.onClick = function()
-  mTab.setPath(File(mTab.getPath()).getParent())
+local path = "/storage/emulated/0/Android/data/com.tencent.mobileqq/Tencent/QQfile_recv/nt_qq_f031d2c4cdb29dce5de1f9f465897ecd/Video/Thumb"
+
+--mTab.setPath(path)
+file_path.setText(path)
+
+ok_button.onClick = function()
+  local p = tostring(file_path.getText())
+  if p == "" then
+    print("空的喵")
+    return
+  end
+  mTab.setPath(p)
 end
+
+check.onClick = function()
+  mTab.setCanSelectTab(check.isChecked())
+  mTab.setPath(mTab.getPath())
+end
+
+--[[mTab.setOnTabSelected(function(tab)
+  print(tab.getText())
+end)]]
